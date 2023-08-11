76a08fddf32668e82f248d9ccbb9649d14d9998f
Vishal Sharma <vishalsharma28784@gmail.com>
2023-08-11 23:00:15 +0530

    Added object in local storage

--- LocalStorage/script.js
+++ LocalStorage/script.js
@@ -2,11 +2,31 @@ let form = document.getElementById('my-form');
 
 form.addEventListener('submit', storeData);
 
-function storeData(e){
-    e.preventDefault();
-   
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    localStorage.setItem(name, email);
+//Adding key value pair to the local storage
 
+// function storeData(e){
+//     e.preventDefault();
+
+//     let name = document.getElementById('name').value;
+//     let email = document.getElementById('email').value;
+//     localStorage.setItem(name, email);
+
+// } 
+
+//Add object in local storage
+
+function storeData(e) {
+    e.preventDefault();
+
+    let userName = document.getElementById('name').value;
+    let userEmail = document.getElementById('email').value;
+
+    let obj = {
+        name: userName,
+        email: userEmail,
+    };
+
+    let obj_serialized = JSON.stringify(obj);
+    localStorage.setItem('my_obj', obj_serialized);
 }
+
