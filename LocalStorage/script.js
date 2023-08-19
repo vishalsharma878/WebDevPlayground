af00517435a11dde8c11454e34e7dc0cf846e72b
Vishal Sharma <vishalsharma28784@gmail.com>
2023-08-19 19:19:04 +0530

    Added Delete Feature

--- LocalStorage/script.js
+++ LocalStorage/script.js
@@ -29,17 +29,18 @@ function storeData(e) {
     axios.post('https://crudcrud.com/api/ed307b10bfcb49d890d0f9982f0a1ef2/formData', obj)
         .then(res => console.log(res))
         .catch(err => alert(err));
-
+    
+    appendDataToList(userName, userEmail, userPhone)    
     // Clear input fields
     document.getElementById('name').value = '';
     document.getElementById('email').value = '';
     document.getElementById('phone').value = '';
 }
 // Append data 
-function appendDataToList(Name, Email, Phone) {
+function appendDataToList(Name, Email, Phone, id) {
 
     let data = document.createElement('li');
-    data.appendChild(document.createTextNode(Name + ' ' + Email + ' ' + Phone));
+    data.appendChild(document.createTextNode('Name:'+ Name + ' ' + 'Email:' +Email + ' ' + 'Phone:' +Phone));
 
     //Edit Button
     let editButton = document.createElement('button');
@@ -53,11 +54,15 @@ function appendDataToList(Name, Email, Phone) {
     });
 
     //Delete Button
+    
     let deleteButton = document.createElement('button');
     deleteButton.appendChild(document.createTextNode('Delete'));
     deleteButton.style.marginLeft = '1px';
     deleteButton.addEventListener('click', function () {
         form.removeChild(data);
+        axios.delete(`https://crudcrud.com/api/ed307b10bfcb49d890d0f9982f0a1ef2/formData/${id}`)
+        .then(res => console.log('User deleted seccessfully', res.data))
+        .catch(err => alert(err));
     });
 
 
@@ -76,7 +81,7 @@ function printData(obj) {
     for (let i = 0; i < obj.length; i++) {
 
         const d = obj[i];
-        appendDataToList(d.name, d.email, d.phone);
+        appendDataToList(d.name, d.email, d.phone, d._id);
     }
 }
 
